10f82e53d449ea8089876744ce8de826586dc27a
Vittor <vittorfranceschi04@gmail.com>
2025-11-18 23:47:45 -0300

    Cors e nova rota para auth/me

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
 import { CartModule } from 'src/cart/cart.module';
 import { OrderModule } from 'src/order/order.module';
@@ -7,6 +6,5 @@ import { OrderModule } from 'src/order/order.module';
 @Module({
   imports: [CartModule, OrderModule],
   controllers: [AccountController],
-  providers: [AccountService],
 })
 export class AccountModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LoginDto } from './dto/login-dto';
 import { UserResponseDto } from 'src/user/dto/user-response.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { UserService } from 'src/user/user.service';
+import { AuthenticatedRequest } from './types/authenticated-request.type';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('register')
   async register(@Body() dto: CreateUserDto) {
@@ -17,5 +31,13 @@ export class AuthController {
   @Post('login')
   async login(@Body() dto: LoginDto) {
     return await this.authService.login(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getOwnUser(@Req() req: AuthenticatedRequest) {
+    const user = await this.userService.findById(req.user.id);
+    if (!user) throw new NotFoundException('User Not Found.');
+    return new UserResponseDto(user);
   }
 }
